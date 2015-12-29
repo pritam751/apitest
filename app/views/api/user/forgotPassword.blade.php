936e3ecd72a10a91c32e2bb92abb2041d0d3ebca
sharma <sharmapritam751@gmail.com>
2015-12-29 14:29:02 +0530

    Adding email view and device model

---
+++ app/views/api/user/forgotPassword.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <div class="container">
+            <h3> Hi {{ $name }}</h3>
+            <div>
+                <p>Your password is reset.</p>
+                <p>New password: <h3>{{ $password }}</h3>
+
+                <p>Thank you !</p>
+            </div>
+            <div>Regards,</div>
+            <div>Support Team</div>
+        </div>
+    </body>
+</html>
